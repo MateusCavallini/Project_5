8b61a1f089567c4ff42d6d79eec1cef596423732
Mateus Cavallin <MateusCavallini@users.noreply.github.com>
2025-10-02 12:37:51 +0100

    Enviando o dataframe

---
+++ app.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+        
+war_data = pd.read_csv('killed-in-gaza.csv') # lendo os dados
+hist_button = st.button('Criar histograma') # criar um botão
+        
+if hist_button: # se o botão for clicado
+            # escrever uma mensagem 
+    st.write('Criando um histograma para o conjunto de dados sobre mortes em Gaza')
+            
+            # criar um histograma
+    fig = px.histogram(war_data, x="age")
+        
+            # exibir um gráfico Plotly interativo
+    st.plotly_chart(fig, use_container_width=True)
